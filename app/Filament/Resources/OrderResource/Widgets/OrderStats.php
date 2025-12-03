6924aaa71d6795458b1f0117157aeeb34aff30c3
Aebriam <Aebriamlawas@gmail.com>
2025-12-03 22:55:36 +0800

    Chapter 22

--- app/Filament/Resources/OrderResource/Widgets/OrderStats.php
+++ app/Filament/Resources/OrderResource/Widgets/OrderStats.php
@@ -2,9 +2,9 @@
 
 namespace App\Filament\Resources\OrderResource\Widgets;
 
+use App\Models\Order;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
-use App\Models\Order;
 use Illuminate\Support\Number;
 
 class OrderStats extends BaseWidget
@@ -15,7 +15,8 @@ class OrderStats extends BaseWidget
             Stat::make('New Orders', Order::query()->where('status', 'new')->count()),
             Stat::make('Order Processing', Order::query()->where('status', 'processing')->count()),
             Stat::make('Order Shipped', Order::query()->where('status', 'shipped')->count()),
-            Stat::make('Average Price', Number::currency(Order::query()->avg('grand_total'),'INR')),
+            Stat::make('Average Price', Number::currency(Order::query()->avg('grand_total') ?? 0, 'PHP'))
+
         ];
     }
 }
